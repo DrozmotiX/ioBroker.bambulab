7215298cfbd393540f418a8a41cfed9f1aac4e47
DutchmanNL <none@none.com>
2023-07-23 21:05:19 +0200

    implement mqtt connection handler

--- main.js
+++ main.js
@@ -37,30 +37,72 @@ class Bambulab extends utils.Adapter {
 		this.setState('info.connection', false, true);
 
 		// Handle MQTT messages
-		// this.config:
+		this.mqttMessageHandle();
 
 	}
 
 	/**
 	 * Handle MQTT connection & message
-		*/
-		await this.setObjectNotExistsAsync('testVariable', {
-			type: 'state',
-			common: {
-				name: 'testVariable',
-				type: 'boolean',
-				role: 'indicator',
-				read: true,
-				write: true,
-			},
-			native: {},
-		});
+	 */
+	mqttMessageHandle(){
+		try {
+
+			this.log.debug(`Try to connect to printer to connect`);
+
+			// Connect to Printer using MQTT
+			client = mqtt.connect(`mqtts://${this.config.host}:8883`, {
+				username: 'bblp',
+				password: this.config.Password,
+				reconnectPeriod: 30,
+				rejectUnauthorized: false,
+			});
 
 			// Establish connection to printer by MQTT
 			client.on('connect', () => {
 
 				this.log.info(`Printer connected`);
 				this.setState('info.connection', true, true);
+
+				// Subscribe on printer topic after connection
+				client.subscribe([`device/${this.config.serial}/report`], () => {
+					this.log.debug(`Subscribed to printer topic by serial`);
+				});
+
+			});
+
+			// Receive MQTT messages
+			client.on('message',  (topic, message) => {
+				// message is Buffer
+				console.debug(message.toString());
+				this.messageHandler(message.toString());
+			});
+
+			client.on('reconnecting',  (topic, message) =>{
+				this.log.info(`Reconnecting ${message.toString()}`);
+			});
+
+			client.on('end',  () =>{
+				this.log.info(`Connection to Printer closed`);
+				this.setState('info.connection', false, true);
+			});
+
+			client.on('error', (error) => {
+				this.log.error(`Connection issue occurred ${error}`);
+				// Close MQTT connection
+				client.end();
+
+				// Try to reconnect
+				if (timeout) {clearTimeout(timeout); timeout = null;}
+				timeout = setTimeout(async function () {
+					client.reconnect();
+				}, 5000);
+
+			});
+
+		} catch (e) {
+			this.log.error(e);
+		}
+	}
 
 	 */
 	messageHandler (message) {
